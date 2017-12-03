b835bd42dc2933b53ff2298e9cbcb02a400e36dd
grekz <vidbla.grekz@gmail.com>
2017-12-03 11:58:01 +0100

    P3LCE-004 - Median of Two Sorted Arrays - Solution. (formatting)

--- mx/grekz/leetcode/easy/E004_MedianofTwoSortedArrays.py
+++ mx/grekz/leetcode/easy/E004_MedianofTwoSortedArrays.py
@@ -18,8 +18,8 @@ class E004_MedianofTwoSortedArrays:
             return self.findMedianSortedArrays(nums2, nums1)
         l = 0
         while l <= h :
-            m1 = int(( l + h ) / 2)
-            m2 = int(( a + b + 1) / 2 - m1)
+            m1 = int((l + h) / 2)
+            m2 = int((a + b + 1) / 2 - m1)
             mla = nums1[m1 - 1] if m1 != 0 else -sys.maxsize - 1
             mlb = nums2[m2 - 1] if m2 != 0 else -sys.maxsize - 1
             mra = nums1[m1] if m1 != a else sys.maxsize
@@ -29,5 +29,5 @@ class E004_MedianofTwoSortedArrays:
             elif mlb > mra:
                 l = m1 + 1
             else:
-                return float(max( mla, mlb )) if ( a + b ) % 2 != 0 else ( max(mla, mlb) + min(mra ,mrb) ) / 2
+                return float(max(mla, mlb)) if (a + b) % 2 != 0 else (max(mla, mlb) + min(mra , mrb)) / 2
         return -1.0

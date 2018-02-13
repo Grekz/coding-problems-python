52c6d75ac4631fea1c0c92a3471b0e02852a8877
grekz <vidbla.grekz@gmail.com>
2018-02-13 05:54:40 +0100

    P3LCE-125. Valid Palindrome

---
+++ mx/grekz/leetcode/easy/E125_ValidPalindrome.py
@@ -0,0 +1,24 @@
+'''
+Created on Feb 13, 2018
+
+@author: grekz
+'''
+
+class E125_ValidPalindrome:
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        i = 0
+        j = len(s) -1
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i +=1
+            while i < j and not s[j].isalnum():
+                j -=1
+            if ( s[i].lower() != s[j].lower() ):
+                return False
+            i+=1
+            j-=1
+        return True

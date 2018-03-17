9350279767dd8a8ec55581155f33f3bd4e1960a3
grekz <vidbla.grekz@gmail.com>
2018-03-17 14:20:14 +0100

    P3LCE-235. Lowest Common Ancestor of a Binary Search Tree

---
+++ mx/grekz/leetcode/easy/E235_LowestCommonAncestorofaBinarySearchTree.py
@@ -0,0 +1,14 @@
+'''
+@author: grekz
+'''
+class E235_LowestCommonAncestorofaBinarySearchTree:
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if root.val < p.val and root.val < q.val : return self.lowestCommonAncestor(root.right, p, q)
+        elif root.val > p.val and root.val > q.val : return self.lowestCommonAncestor(root.left, p, q)
+        return root

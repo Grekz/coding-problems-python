fd2b6e1c344ae7961fb7a1285b58cc267774843c
grekz <vidbla.grekz@gmail.com>
2018-03-27 06:38:58 +0200

    P3LCE-371. Sum of Two Integers

---
+++ mx/grekz/leetcode/easy/E371_SumofTwoIntegers.py
@@ -0,0 +1,16 @@
+'''
+@author: grekz
+'''
+class E371_SumofTwoIntegers:
+
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        MAX = 0x7FFFFFFF
+        mask = 0xFFFFFFFF
+        while b != 0:
+            a, b = (a ^ b) & mask, ((a & b) << 1) & mask
+        return a if a <= MAX else ~(a ^ mask)

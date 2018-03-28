096b5383f634ee47fb7fd6d246c6a67b5cbb2418
grekz <vidbla.grekz@gmail.com>
2018-03-28 19:40:55 +0200

    P3LCE-383. Ransom Note

---
+++ mx/grekz/leetcode/easy/E383_RansomNote.py
@@ -0,0 +1,14 @@
+'''
+@author: grekz
+'''
+import collections
+
+class E383_RansomNote:
+
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        return not collections.Counter(ransomNote) - collections.Counter(magazine)

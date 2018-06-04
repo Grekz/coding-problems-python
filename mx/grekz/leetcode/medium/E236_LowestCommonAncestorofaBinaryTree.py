b1efc84b356ce65348b330d17cab613e175e5956
grekz <vidbla.grekz@gmail.com>
2018-06-04 22:08:16 +0200

    P3LCE-236. Lowest Common Ancestor of a Binary Tree

---
+++ mx/grekz/leetcode/medium/E236_LowestCommonAncestorofaBinaryTree.py
@@ -0,0 +1,18 @@
+'''
+@author: grekz
+'''
+class E236_LowestCommonAncestorofaBinaryTree:
+
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if not root or root == p or root == q : return root
+        l = self.lowestCommonAncestor(root.left, p, q)
+        r = self.lowestCommonAncestor(root.right, p, q)
+        if l and r : return root
+        if l : return l
+        return r

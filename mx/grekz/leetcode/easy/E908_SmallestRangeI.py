3244520d18a7c5bc2889d750cf2db94a6aed81db
Grekz <vidbla.grekz@gmail.com>
2019-01-05 22:00:47 +0100

    P3LCE-908. Smallest Range I

---
+++ mx/grekz/leetcode/easy/E908_SmallestRangeI.py
@@ -0,0 +1,13 @@
+'''
+@author: grekz
+'''
+
+
+class E908_SmallestRangeI:
+    def smallestRangeI(self, A, K):
+        """
+        :type A: List[int]
+        :type K: int
+        :rtype: int
+        """
+        return max(0, max(A) - min(A) - 2 * K)

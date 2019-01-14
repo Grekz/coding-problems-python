fdfaf1e2375fdbff6f5eaa8a06320088abc3a96d
Grekz <vidbla.grekz@gmail.com>
2019-01-14 08:07:51 +0100

    P3LCE-701. Insert into a Binary Search Tree

---
+++ mx/grekz/leetcode/medium/E701_InsertintoaBinarySearchTree.py
@@ -0,0 +1,19 @@
+'''
+@author: grekz
+'''
+
+
+class E701_InsertintoaBinarySearchTree:
+    def insertIntoBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return TreeNode(val)
+        if val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+        return root

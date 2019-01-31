3abd8ffe58b9a93d7e4ca8128451c69024f9cf3e
Grekz <vidbla.grekz@gmail.com>
2019-01-31 21:12:10 +0100

    P3LCE-877. Stone Game

---
+++ mx/grekz/leetcode/medium/E877_StoneGame.py
@@ -0,0 +1,13 @@
+'''
+@author: grekz
+'''
+
+
+class E877_StoneGame:
+
+    def stoneGame(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: bool
+        """
+        return True

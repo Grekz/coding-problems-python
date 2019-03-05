a1b1980c58bc46c197a510482c91514e27048b4f
Grekz <vidbla.grekz@gmail.com>
2019-03-05 22:25:57 +0100

    P3LCE-142. Linked List Cycle II

--- mx/grekz/leetcode/medium/E142_LinkedListCycleII.py
+++ mx/grekz/leetcode/medium/E142_LinkedListCycleII.py
@@ -2,3 +2,18 @@
 @author: grekz
 '''
 class E142_LinkedListCycleII:
+
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        slow, fast = head, head
+        while fast and fast.next :
+            slow, fast = slow.next, fast.next.next
+            if fast == slow :
+                slow = head
+                while slow != fast :
+                    slow, fast = slow.next, fast.next
+                return slow          
+        return None

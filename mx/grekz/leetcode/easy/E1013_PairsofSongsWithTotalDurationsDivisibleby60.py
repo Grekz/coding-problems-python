1be21d7c35be65d4c0d65a7b66ab7177f9ccbd40
Grekz <vidbla.grekz@gmail.com>
2019-03-17 23:21:43 +0100

    P3LCE-1013. Pairs of Songs With Total Durations Divisible by 60

---
+++ mx/grekz/leetcode/easy/E1013_PairsofSongsWithTotalDurationsDivisibleby60.py
@@ -0,0 +1,11 @@
+'''
+@author: grekz
+'''
+class E1013_PairsofSongsWithTotalDurationsDivisibleby60:
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        res, cnt = 0, [0] * 60
+        for x in time :
+            d = (60 - x % 60) % 60
+            res += cnt[d]
+            cnt[x % 60] += 1
+        return res
